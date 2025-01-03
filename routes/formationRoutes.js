2105f9815bf12fc74ccb929847052262ae5cce67
Thanus Kumaar <111196217+Thanus-Kumaar@users.noreply.github.com>
2025-01-03 23:39:32 +0530

    Update formationRoutes.js

--- routes/formationRoutes.js
+++ routes/formationRoutes.js
@@ -2,6 +2,6 @@ const express=require("express");
 const router=express.Router();
 const {getFormations} =require("../controllers/formationController")
 
-router.get("/formations",getFormations)
+router.get("/",getFormations)
 
 module.exports=router;
